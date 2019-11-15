a7e6683f1ffbe1728842d4a440c9fa42e494deef
Michael Di Giacomi <michdg@amazon.com>
2019-11-15 20:35:44 +0000

    Writing to bucket

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 var fs = require('fs');
 var Crawler = require("crawler");
 var htmlparser2 = require("htmlparser2");
+var AWS = require('aws-sdk');
+
 var c = new Crawler();
+var pageContents = "";
 
 var parser = new htmlparser2.Parser(
     {
@@ -10,27 +13,48 @@ var parser = new htmlparser2.Parser(
             if(/^\s*$/.test(text)) {}
             else
             {
-                console.log(text);
+                //console.log(text);
+                pageContents = pageContents + text;
             }
         }
     },
-    { decodeEntities: true }
+    { 
+        decodeEntities: true,
+        xmlMode: true,
+        recognizeSelfClosing: true
+    }
 );
 
 
 
 exports.handler = function(event, context, callback){
     c.direct({
-        uri: 'https://www.hipsterconsulting.com',
+        uri: 'https://aws.amazon.com/service-terms/',
         skipEventRequest: false, // default to true, direct requests won't trigger Event:'request'
         callback: function(error, response) {
             if(error) {
                 console.log(error)
             } else {
+                console.log(response);
                 parser.write(response.body);
                 parser.end();
+                putObjectToS3("page-scrape-data","test/termsofservice.txt", pageContents, callback);
             }
-            callback(null, "Done");
+            
         }
     });
 };
+
+function putObjectToS3(bucket, key, data, callback){
+    var s3 = new AWS.S3();
+        var params = {
+            Bucket : bucket,
+            Key : key,
+            Body : data
+        }
+        s3.putObject(params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+          callback(null, "Done");
+        });
+}
